b20f863c423edb127c9420044eea46997ca920c5
nashalirivera1596 <nashali.rivera1@upr.edu>
2020-05-05 23:23:28 -0400

    Update items.py

--- handler/items.py
+++ handler/items.py
@@ -5,47 +5,21 @@ from dao.parts import ItemsDAO
 class ItemHandler:
     def build_item_dict(self, row):
         result = {}
-        result['item_id'] = row[0]
-        result['resource_name'] = row[1]
-        result['brand'] = row[2]
-        result['item_latitud'] = row[3]
-        result['item_longitud'] = row[4]
-        result['expiration_date'] = row[5]
-        result['price'] = row[6]
-        result['type'] = row[7]
-        result['amount'] = row[8]
         return result
 
     def build_person_dict(self, row):
         result = {}
-        result['person_id'] = row[0]
-        result['person_name'] = row[1]
-        result['person_latitud'] = row[2]
-        result['person_longitud'] = row[3]
-        result['phone_num'] = row[4]
-        result['card'] = row[5]
-        result['gender'] = row[6]
-        result['age'] = row[7]
         return result
 
-    def build_part_attributes(self, item_id, resource_name, brand, item_latitud, item_longitud, expiration_date, price, type, amount):
+    def build_part_attributes(self, item_id, resource_name, brand, item_latitude, item_longitude, expiration_date, price, type, amount):
         result = {}
-        result['item_id'] = item_id
-        result['resource_name'] = resource_name
-        result['brand'] = brand
-        result['item_latitud'] = item_latitud
-        result['item_longitud'] = item_longitud
-        result['expiration_date'] = expiration_date
-        result['price'] = price
-        result['type'] = type
-        result['amount'] = amount
         return result
 
     def getAllItems(self):
         dao = ItemsDAO()
-        parts_list = dao.getAllItems()
+        items_list = dao.getAllItems()
         result_list = []
-        for row in parts_list:
+        for row in items_list:
             result = self.build_item_dict(row)
             result_list.append(result)
         return jsonify(Items=result_list)
@@ -61,20 +35,20 @@ class ItemHandler:
 
 ##################################################################
     def searchItems(self, args):
-        color = args.get("color")
-        material = args.get("material")
+        typeOfItem = args.get("type")
+        resourceName = args.get("resourceName")
         dao = ItemsDAO()
-        parts_list = []
-        if (len(args) == 2) and color and material:
-            parts_list = dao.getPartsByColorAndMaterial(color, material)
-        elif (len(args) == 1) and color:
-            parts_list = dao.getPartsByColor(color)
-        elif (len(args) == 1) and material:
-            parts_list = dao.getPartsByMaterial(material)
+        items_list = []
+        if (len(args) == 2) and typeOfItem and resourceName:
+            items_list = dao.getPartsByColorAndMaterial(typeOfItem, resourceName)
+        elif (len(args) == 1) and typeOfItem:
+            items_list = dao.getPartsByColor(typeOfItem)
+        elif (len(args) == 1) and resourceName:
+            items_list = dao.getPartsByMaterial(resourceName)
         else:
             return jsonify(Error = "Malformed query string"), 400
         result_list = []
-        for row in parts_list:
+        for row in items_list:
             result = self.build_part_dict(row)
             result_list.append(result)
         return jsonify(Parts=result_list)
@@ -90,37 +64,37 @@ class ItemHandler:
             result_list.append(result)
         return jsonify(Suppliers=result_list)
 
-    def insertPart(self, form):
+    def insertItem(self, form):
         print("form: ", form)
         if len(form) != 4:
             return jsonify(Error = "Malformed post request"), 400
         else:
-            pname = form['pname']
-            pprice = form['pprice']
-            pmaterial = form['pmaterial']
-            pcolor = form['pcolor']
-            if pcolor and pprice and pmaterial and pname:
+            itemResourceName = form['Item Resource Name']
+            iprice = form['item price']
+            itype = form['item type']
+            ibrand = form['item brand']
+            if ibrand and iprice and itype and itemResourceName:
                 dao = PartsDAO()
-                pid = dao.insert(pname, pcolor, pmaterial, pprice)
-                result = self.build_part_attributes(pid, pname, pcolor, pmaterial, pprice)
+                pid = dao.insert(itemResourceName, ibrand, itype, iprice)
+                result = self.build_part_attributes(pid, itemResourceName, ibrand, itype, iprice)
                 return jsonify(Part=result), 201
             else:
                 return jsonify(Error="Unexpected attributes in post request"), 400
 
-    def insertPartJson(self, json):
-        pname = json['pname']
-        pprice = json['pprice']
-        pmaterial = json['pmaterial']
-        pcolor = json['pcolor']
-        if pcolor and pprice and pmaterial and pname:
+    def insertItemJson(self, json):
+        itemResourceName = json['Item Resource Name']
+        iprice = json['item price']
+        itype = json['item type']
+        ibrand = json['item brand']
+        if ibrand and iprice and itype and itemResourceName:
             dao = PartsDAO()
-            pid = dao.insert(pname, pcolor, pmaterial, pprice)
-            result = self.build_part_attributes(pid, pname, pcolor, pmaterial, pprice)
+            pid = dao.insert(itemResourceName, ibrand, itype, iprice)
+            result = self.build_part_attributes(pid, itemResourceName, ibrand, itype, iprice)
             return jsonify(Part=result), 201
         else:
             return jsonify(Error="Unexpected attributes in post request"), 400
 
-    def deletePart(self, pid):
+    def deleteItem(self, pid):
         dao = PartsDAO()
         if not dao.getPartById(pid):
             return jsonify(Error = "Part not found."), 404
@@ -136,13 +110,13 @@ class ItemHandler:
             if len(form) != 4:
                 return jsonify(Error="Malformed update request"), 400
             else:
-                pname = form['pname']
-                pprice = form['pprice']
-                pmaterial = form['pmaterial']
-                pcolor = form['pcolor']
-                if pcolor and pprice and pmaterial and pname:
-                    dao.update(pid, pname, pcolor, pmaterial, pprice)
-                    result = self.build_part_attributes(pid, pname, pcolor, pmaterial, pprice)
+                itemResourceName = form['Item Resource Name']
+                iprice = form['item price']
+                itype = form['item type']
+                ibrand = form['item brand']
+                if ibrand and iprice and itype and itemResourceName:
+                    dao.update(pid, itemResourceName, ibrand, itype, iprice)
+                    result = self.build_part_attributes(pid, itemResourceName, ibrand, itype, iprice)
                     return jsonify(Part=result), 200
                 else:
                     return jsonify(Error="Unexpected attributes in update request"), 400
